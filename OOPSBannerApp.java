30b15d7de63c085403b2b58838087e36f9c48323
saskhamroy <roy.saksham21@gmail.com>
2026-02-24 14:25:59 +0530

    UC2: Display OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,8 +2,14 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * ***** ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println(" *****   *****   *****   *      ");
+
     }
 }
-    
-
